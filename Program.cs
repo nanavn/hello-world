c40771c4841af30f3257c1749334ec63da87daee
nanavn <73819643+nanavn@users.noreply.github.com>
2022-12-17 17:43:27 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yp_3
+{
+    class Program
+    {
+        /// <summary>
+        /// выводид строку
+        /// </summary>
+        static void OutputWord()
+        {
+            Console.WriteLine("Hi");
+        }
+        static void Main(string[] args)
+        {
+            OutputWord();
+            Console.ReadLine();
+        }
+    }
+}
